e1c027ca1596fad94b8e1d51852b5c66718c85c9
vessos <vinsent_vsm@abv.bg>
2021-09-06 13:14:01 +0300

    implements VetEntity and relationships from them.closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,10 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "ccet_specialties",joinColumns = @JoinColumn(name = "vet_id")
+            ,inverseJoinColumns = @JoinColumn(name = "specialities_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
